f729270b69a20c3d54e7b17a9c35a2e6ec4243c5
Raboro <marius.woerfelreal@gmail.com>
2023-12-30 16:41:21 +0100

    added methods and structs to collect uml objects of files

--- src/file_scanning.h
+++ src/file_scanning.h
@@ -4,6 +4,16 @@
 #define FILE_SCANNING_H
 
 /**
+ * @brief type, which holds the information about an UML object, collected out of a file
+ * this is needed for the parsing into UML
+ */
+typedef struct uml_obj
+{
+    char *path;
+    char *name;
+} uml_obj_t;
+
+/**
  * @brief counts all relevant files for the UML conversions
  * @param root_path char*; root path of the whole analysis to count all relevant files
  * @return int; number of all files, which are relevant for the conversion into UML
@@ -32,5 +42,21 @@ int is_dir(struct dirent dirent);
  * @return if file 1, else 0
  */
 int is_file(struct dirent dirent);
+
+/**
+ * @brief collects all information of all UML objects
+ * @param root_path char*; root path of the whole analysis to collect UML objects
+ * @param uml_objects uml_obj_t*; array of UML objects to be collected in this method
+ * @param counter int*; current selected file
+ */
+void collect_uml_objects(char *root_path, uml_obj_t *uml_objects, int *counter);
+
+/**
+ * @brief parse dirent information into UML object at counter
+ * @param dirent dirent*; information object of the current file
+ * @param uml_objects uml_obj_t*; array of UML objects; at position counter will be data inserted in this method
+ * @param counter int*; current selected file
+ */
+void insert_file(struct dirent *dirent, uml_obj_t *uml_objects, int *counter);
 
 #endif
