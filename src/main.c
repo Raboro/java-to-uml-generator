670c1bb42dda0f7d6a0afb081ae40763373dd2b8
Raboro <marius.woerfelreal@gmail.com>
2023-12-30 16:42:20 +0100

    used collect of uml objects

--- src/main.c
+++ src/main.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "file_scanning.h"
 
 int main()
 {
     int file_counter = count_files(".");
-    printf("%d", file_counter);
+    printf("%d\n", file_counter);
+
+    uml_obj_t uml_objects[file_counter];
+    int counter = 0;
+    collect_uml_objects(".", uml_objects, &counter);
+
+    for (int i = 0; i < counter; i++)
+    {
+        printf("File %d: %s with path: %s\n", i + 1, uml_objects[i].name, uml_objects[i].path);
+        free(uml_objects[i].name);
+        free(uml_objects[i].path);
+    }
+
     return 0;
 }
