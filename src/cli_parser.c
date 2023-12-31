278ef6306071fb28434e21ec53b6307027626b67
Raboro <marius.woerfelreal@gmail.com>
2023-12-31 16:25:29 +0100

    added path argument

--- src/cli_parser.c
+++ src/cli_parser.c
@@ -8,6 +8,7 @@ typedef struct cli_parse_obj
 
 static struct option options[] = {
     {"help", no_argument, 0, 'h'},
+    {"path", required_argument, 0, 'p'},
     {0, 0, 0, 0}};
 
 void print_help()
@@ -15,6 +16,7 @@ void print_help()
     printf("Usage:\n  jtuml -h\n");
     printf("Options:\n");
     printf("  -h, --help     Display the help message\n");
+    printf("  -p, --path     Root path to analyze\n");
 }
 
 void parse(cli_parse_obj_t *cli_parse_obj, int argc, char *argv[])
@@ -27,12 +29,16 @@ void parse(cli_parse_obj_t *cli_parse_obj, int argc, char *argv[])
         return;
     }
 
-    while (-1 != (opt = getopt_long(argc, argv, "h", options, NULL)))
+    while (-1 != (opt = getopt_long(argc, argv, "hp:", options, NULL)))
     {
         switch (opt)
         {
         case 'h':
             print_help();
+            break;
+
+        case 'p':
+            cli_parse_obj->root_path = optarg;
             break;
 
         default:
